5f47e8a2a5fe31ddc810397690f029b49769521a
daneltabada <daneltabada@gmail.com>
2018-09-04 15:21:04 -0400

    configured back to run in localhost

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ var express = require ('express'),
 
 // APP CONFIG
 //local
-//mongoose.connect("mongodb://localhost:27017/restful_blog_app", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/restful_blog_app", { useNewUrlParser: true });
 //mLab
-mongoose.connect("mongodb://danel:1babylove@ds243212.mlab.com:43212/restful_blog_app",{ useNewUrlParser: true });
+// mongoose.connect("mongodb://danel:1babylove@ds243212.mlab.com:43212/restful_blog_app",{ useNewUrlParser: true });
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -123,10 +123,10 @@ app.delete("/blogs/:id", function(req, res){
 });
 
 // LISTEN TO A PORT
-// app.listen(3000, function(){
-//       console.log("SERVER IS UP! LISTENING AT PORT 3000")  
-// });
-
-app.listen(process.env.PORT, function(){
-      console.log("SERVER IS UP!")  
+app.listen(3000, function(){
+      console.log("SERVER IS UP! LISTENING AT PORT 3000")  
 });
+
+// app.listen(process.env.PORT, process.env.IP, function(){
+//       console.log("SERVER IS UP!")  
+// });
